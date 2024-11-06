450335cf43b5b2c998b50af88fd627f0020ca910
yasiaa <huhiak1@gmail.com>
2024-11-06 17:31:34 +0100

    zrobione zad14

--- PP3/zad14.py
+++ PP3/zad14.py
@@ -1,5 +1,19 @@
-inp = input()
-uni_litery = list(filter(lambda char: char.isalpha() and inp.count(char) == 1, set(inp)))
+import random
+import string
 
-print(uni_litery)
-print(inp)
+key_len = 10
+max_dict_size = 7
+max_list_size = 3
+max_number = 10
+
+my_dict = {
+    ''.join(random.choices(string.ascii_letters + string.digits, k=key_len)): 
+    [random.randint(1, max_number) for _ in range(random.randint(1, max_list_size))] for _ in range (random.randint(1, max_dict_size))
+    }
+sort_dict = dict(sorted(my_dict.items(), key=lambda pair: sum(pair[1])))
+sum_dict = {key: sum(val) for key, val in sort_dict.items()}
+print(my_dict)
+print(sort_dict)
+print(sum_dict)
+
+print(f"czy dobrze posortowano: {'tak' if all(x <= y for x, y in zip(list(sum_dict.values())[:-1], list(sum_dict.values())[1:])) else 'nie'}")
